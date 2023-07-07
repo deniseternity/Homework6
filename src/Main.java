a3387878d3d3e5b149bb5eecd0b7e3a655f51605
Magay Denis <qwertydenis009@gmail.com>
2023-07-07 17:55:17 +0300

    Задачи 1-10

---
+++ src/Main.java
@@ -0,0 +1,91 @@
+public class Main {
+}
+public static void main(String[] args) {
+
+    public static void ask1() {
+        System.out.println("Задача 1");
+    }
+
+    for (int i = 0; i <= 10; i = i + 1) {
+        System.out.println(i);
+    }
+
+        public static void ask2() {
+            System.out.println("Задача 2");
+        }
+
+    for (int i = 10; i >= 0; i = i - 1) {
+        System.out.println(i);
+    }
+
+        public static void ask3() {
+        System.out.println("Задача 3");
+    }
+
+    for (int i = 0; i <= 16; i = i + 2) {
+        System.out.println(i);
+    }
+         public static void ask4() {
+        System.out.println("Задача 4");
+    }
+
+    for (int i = 10; i >= -10; i = i - 1) {
+        System.out.println(i);
+    }
+
+         public static void ask5() {
+        System.out.println("Задача 5");
+    }
+
+    for (int i = 1904; i <= 2096; i = i + 4) {
+        System.out.println(+i + "год является високосным");
+    }
+
+          public static void ask6() {
+        System.out.println("Задача 6");
+    }
+
+    for (int i = 7; i <= 98; i = i + 7) {
+        System.out.println(i);
+    }
+
+    public static void ask7() {
+        System.out.println("Задача 7");
+    }
+
+    for (int i = 1; i <= 512; i = i * 2) {
+    System.out.println(i);
+    }
+
+    public static void ask8() {
+        System.out.println("Задача 8");
+    }
+
+    int salary = 29000;
+    int total = 0;
+    for (int i = 0; i < 12; i++) {
+        total = total + salary;
+        System.out.println("Месяц" + i + ", сумма накоплений равна" + total + "рублей");
+    }
+    System.out.println(total);
+
+    public static void ask9) {
+        System.out.println("Задача 9");
+    }
+
+    int salary = 29000;
+    int total = 0;
+    for (int i = 0; i < 12; i++) {
+        total = total + total/100;
+        total = total + salary;
+        System.out.println("Месяц" + i + ", сумма накоплений равна" + total + "рублей");
+    }
+    System.out.println(total);
+
+    public static void ask10) {
+        System.out.println("Задача 10");
+    }
+
+    for (int i = 1; i <= 10; i = i * 2) {
+    System.out.println(i);
+}
