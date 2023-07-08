a6d64add6a124e606438eee274742ab8e4d25881
Magay Denis <qwertydenis009@gmail.com>
2023-07-08 18:51:38 +0300

    Задачи 1-10

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,48 @@
 public class Main {
-}
 public static void main(String[] args) {
-
-    public static void ask1() {
         System.out.println("Задача 1");
-    }
 
     for (int i = 0; i <= 10; i = i + 1) {
         System.out.println(i);
     }
 
-        public static void ask2() {
             System.out.println("Задача 2");
-        }
 
     for (int i = 10; i >= 0; i = i - 1) {
         System.out.println(i);
     }
 
-        public static void ask3() {
         System.out.println("Задача 3");
-    }
 
-    for (int i = 0; i <= 16; i = i + 2) {
+    for (int i = 0; i <= 17; i = i + 2) {
         System.out.println(i);
     }
-         public static void ask4() {
+
         System.out.println("Задача 4");
-    }
 
     for (int i = 10; i >= -10; i = i - 1) {
         System.out.println(i);
     }
 
-         public static void ask5() {
         System.out.println("Задача 5");
-    }
 
     for (int i = 1904; i <= 2096; i = i + 4) {
         System.out.println(+i + "год является високосным");
     }
 
-          public static void ask6() {
         System.out.println("Задача 6");
-    }
 
     for (int i = 7; i <= 98; i = i + 7) {
         System.out.println(i);
     }
 
-    public static void ask7() {
         System.out.println("Задача 7");
-    }
 
     for (int i = 1; i <= 512; i = i * 2) {
     System.out.println(i);
     }
 
-    public static void ask8() {
         System.out.println("Задача 8");
-    }
 
     int salary = 29000;
     int total = 0;
@@ -69,23 +52,25 @@ public static void main(String[] args) {
     }
     System.out.println(total);
 
-    public static void ask9) {
         System.out.println("Задача 9");
-    }
 
     int salary = 29000;
     int total = 0;
+    int yearPercent = 12;
+    int monthPercent = yearPercent / 12
     for (int i = 0; i < 12; i++) {
-        total = total + total/100;
+        total = total + total * monthPercent / 100 + salary
         total = total + salary;
         System.out.println("Месяц" + i + ", сумма накоплений равна" + total + "рублей");
     }
     System.out.println(total);
 
-    public static void ask10) {
+    public static void ask10){
         System.out.println("Задача 10");
-    }
 
-    for (int i = 1; i <= 10; i = i * 2) {
-    System.out.println(i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2*" + i + "=" +  i*2);
+        }
+
+    }
 }
